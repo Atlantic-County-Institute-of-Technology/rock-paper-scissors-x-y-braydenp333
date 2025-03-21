effbe94e4d5c2fb419178bd2b0f68f688eed7868
braydenp333 <papa0313@student.acitech.org>
2025-03-21 09:12:43 -0400

    added title

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const text = document.getElementById("text");
 const rock = document.getElementById("rock");
 const container = document.getElementById("container");
+const player = document.getElementById("playerversus");
+const computer = document.getElementById("cpuversus");
 let clicks = new Audio('assets/click.mp3');
 
 const button_list = document.querySelectorAll("button");
@@ -10,8 +12,14 @@ for(let i = 0; i < button_list.length; i++)
 {
     button_list[i].addEventListener("click", clicks);
 }
-
 function clicks() {
     clicks.play;
 }
+if player chose 3
+    if cpu chose 2 or cpu chose 4
+        "Player wins!"
+    else if cpu chose 3
+        "Tie"
+    else
+        "Player loses!"
 
