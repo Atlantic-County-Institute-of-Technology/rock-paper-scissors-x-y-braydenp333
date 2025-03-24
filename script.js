5892565daa67f6c4c2c29298a0b8d4b0239df3f0
braydenp333 <papa0313@student.acitech.org>
2025-03-24 08:59:13 -0400

    started js, added feature to test buttons

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-const text = document.getElementById("text");
-const rock = document.getElementById("rock");
-const container = document.getElementById("container");
-const player = document.getElementById("playerversus");
-const computer = document.getElementById("cpuversus");
-let clicks = new Audio('assets/click.mp3');
+const Rock = document.getElementById("Rock");
+const Paper = document.getElementById("Paper");
+const Scissors = document.getElementById("Scissors");
+const x = document.getElementById("x");
+const y = document.getElementById("y");
+let userpick = "";
 
-const button_list = document.querySelectorAll("button");
+function userChoiceRock() {
+    userpick = 1;
+        alert(userpick)
+}
+rock.addEventListener("click", userChoiceRock)
+
+function userChoicePaper() {
+    userpick = 2;
+        alert(userpick)
+}
+paper.addEventListener("click", userChoicePaper)
+
+function userChoiceScissors() {
+    userpick = 3;
+        alert(userpick)
+}
+scissors.addEventListener("click", userChoiceScissors)
+
+function userChoiceX() {
+    userpick = 4;
+        alert(userpick)
+}
+x.addEventListener("click", userChoiceX)
+
+function userChoiceY() {
+    userpick = 5;
+        alert(userpick)
+}
+y.addEventListener("click", userChoiceY)
+
+
+/* const button_list = document.querySelectorAll("button");
 console.log(button_list);
 
 for(let i = 0; i < button_list.length; i++)
@@ -22,4 +53,4 @@ if player chose 3
         "Tie"
     else
         "Player loses!"
-
+*/
