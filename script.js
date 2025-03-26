b4ee9c186bbd63d631c34e1bc1b435a10c3fe962
braydenp333 <papa0313@student.acitech.org>
2025-03-26 09:00:31 -0400

    winner check and score dont function and the game

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-const Rock = document.getElementById("Rock");
-const Paper = document.getElementById("Paper");
-const Scissors = document.getElementById("Scissors");
-const x = document.getElementById("x");
-const y = document.getElementById("y");
-let userpick = "";
+const Rockbtn = document.getElementById("Rock");
+const Paperbtn = document.getElementById("Paper");
+const Scissorsbtn = document.getElementById("Scissors");
+const xbtn = document.getElementById("x");
+const ybtn = document.getElementById("y");
+let playerscore = document.getElementById("playscorebutton");
+let cpuscore = document.getElementById("cpuscorebutton");
+var userpick = "";
+var computerchoice = "";
+
+
+function computer() {
+    computerchoice = Math.floor(Math.random() * 5);
+    consolelog(computerchoice)
+}
+
 
 function userChoiceRock() {
     userpick = 1;
@@ -34,6 +44,77 @@ function userChoiceY() {
         alert(userpick)
 }
 y.addEventListener("click", userChoiceY)
+
+if (userpick == 1) {
+    if (computerchoice = 1) {
+        alert("Tie")
+        playerscore++
+        playerscore.innerHTML = "PLAYER SCORE:"
+    }
+    else if (computerchoice = 3 || 5) {
+        alert("Player Wins!")
+        playerscore++
+    }
+    else {
+        alert ("Computer Wins!")
+        cpuscore++
+    }
+}
+if (userpick == 2) {
+    if (computerchoice = 2) {
+        alert("Tie")
+    }
+    else if (computerchoice = 1 || 4) {
+        alert("Player Wins!")
+        playerscore++
+    }
+    else {
+        alert ("Computer Wins!")
+        cpuscore++
+    }
+}
+if (userpick == 3) {
+    if (computerchoice = 3) {
+        alert("Tie")
+    }
+    else if (computerchoice = 2 || 5) {
+        alert("Player Wins!")
+        playerscore++
+    }
+    else {
+        alert ("Computer Wins!")
+        cpuscore++
+    }
+}
+if (userpick == 4) {
+    if (computerchoice = 4) {
+        alert("Tie")
+    }
+    else if (computerchoice = 1 || 3) {
+        alert("Player Wins!")
+        playerscore++
+    }
+    else {
+        alert ("Computer Wins!")
+        cpuscore++
+    }
+}
+if (userpick == 5) {
+    if (computerchoice = 5) {
+        alert("Tie")
+    }
+    else if (computerchoice = 2 || 4) {
+        alert("Player Wins!")
+        playerscore++
+    }
+    else {
+        alert ("Computer Wins!")
+        cpuscore++
+    }
+}
+
+
+
 
 
 /* const button_list = document.querySelectorAll("button");
