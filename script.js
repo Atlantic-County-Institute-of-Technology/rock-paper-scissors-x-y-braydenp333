a8df0809f7faa9763bb36f9ba3aa76d72bff692f
braydenp333 <papa0313@student.acitech.org>
2025-03-28 09:10:39 -0400

    Still dont work

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const Paperbtn = document.getElementById("Paper");
 const Scissorsbtn = document.getElementById("Scissors");
 const xbtn = document.getElementById("x");
 const ybtn = document.getElementById("y");
+const buttons = document.querySelectorAll('button');
 let playerscore = document.getElementById("playscorebutton");
 let cpuscore = document.getElementById("cpuscorebutton");
 var userpick = "";
 var computerchoice = "";
-
-
-function computer() {
-    computerchoice = Math.floor(Math.random() * 5);
-    consolelog(computerchoice)
+/* Computer Math */
+function computerchoice () {
+    Math.random(5)
 }
 
 
@@ -46,12 +45,11 @@ function userChoiceY() {
 y.addEventListener("click", userChoiceY)
 
 if (userpick == 1) {
-    if (computerchoice = 1) {
+    if (computerchoice == 1) {
         alert("Tie")
         playerscore++
-        playerscore.innerHTML = "PLAYER SCORE:"
     }
-    else if (computerchoice = 3 || 5) {
+    else if (computerchoice == 3 || 5) {
         alert("Player Wins!")
         playerscore++
     }
@@ -61,10 +59,10 @@ if (userpick == 1) {
     }
 }
 if (userpick == 2) {
-    if (computerchoice = 2) {
+    if (computerchoice == 2) {
         alert("Tie")
     }
-    else if (computerchoice = 1 || 4) {
+    else if (computerchoice == 1 || 4) {
         alert("Player Wins!")
         playerscore++
     }
@@ -74,10 +72,10 @@ if (userpick == 2) {
     }
 }
 if (userpick == 3) {
-    if (computerchoice = 3) {
+    if (computerchoice == 3) {
         alert("Tie")
     }
-    else if (computerchoice = 2 || 5) {
+    else if (computerchoice == 2 || 5) {
         alert("Player Wins!")
         playerscore++
     }
@@ -87,10 +85,10 @@ if (userpick == 3) {
     }
 }
 if (userpick == 4) {
-    if (computerchoice = 4) {
+    if (computerchoice == 4) {
         alert("Tie")
     }
-    else if (computerchoice = 1 || 3) {
+    else if (computerchoice == 1 || 3) {
         alert("Player Wins!")
         playerscore++
     }
@@ -100,10 +98,10 @@ if (userpick == 4) {
     }
 }
 if (userpick == 5) {
-    if (computerchoice = 5) {
+    if (computerchoice == 5) {
         alert("Tie")
     }
-    else if (computerchoice = 2 || 4) {
+    else if (computerchoice == 2 || 4) {
         alert("Player Wins!")
         playerscore++
     }
@@ -112,26 +110,3 @@ if (userpick == 5) {
         cpuscore++
     }
 }
-
-
-
-
-
-/* const button_list = document.querySelectorAll("button");
-console.log(button_list);
-
-for(let i = 0; i < button_list.length; i++)
-{
-    button_list[i].addEventListener("click", clicks);
-}
-function clicks() {
-    clicks.play;
-}
-if player chose 3
-    if cpu chose 2 or cpu chose 4
-        "Player wins!"
-    else if cpu chose 3
-        "Tie"
-    else
-        "Player loses!"
-*/
